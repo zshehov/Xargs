fced6e0e6417263fa0e883b67196da3821765da4
Zdravko <z.shehov@abv.bg>
2017-07-06 23:15:13 +0200

    slight changes to main.c
    moved the code for executing into a function; changed variable name from " i " to " current_arg "; changed declaration order for more efficient alignment; some cosmetic changes

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include<err.h>
+#include <err.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -9,80 +9,71 @@
 #include <stdlib.h>
 #include "vector.h"
 
-int main(int argc,char* argv[]){
-	char* stdin[4];//array collecting 3 arguments for command
-	stdin[3]=(char*)NULL;
-	vector buff;//vector for reading a word
-	init(&buff,20);
-	char* final_arr[argc+3];//combination of argv+stdin
-	int argc_new=argc;
+void exec_command(char** final_arr, char** stdin, int* current_arg, int argc_new){
+	int pid;
+	if ( ( pid = fork() ) == 0){ // in the child process
 
-	if(argc==1){//command to execute is echo
-		final_arr[0]="echo";
-		argc_new=2;
+		for(int j = 0; j <= *current_arg; ++j)
+			final_arr[j + argc_new - 1] = stdin[j];	
+				
+		execvp(final_arr[0], final_arr);
+	}
+	
+	int status;
+	wait(&status);
+	*current_arg = 0;
+
+	for(int k = 0; k < *current_arg; ++k)
+		free(stdin[k]);
+}
+
+int main(int argc, char* argv[]){
+	vector buff;//vector for reading a word
+	init(&buff, 20); // initializes the vector structure with variable capacity
+	
+	char* stdin[4];//array collecting 3 arguments for command
+	stdin[3] = (char*)NULL;
+	
+	char* final_arr[argc + 3];//combination of argv+stdin
+	int argc_new = argc;
+	
+	int word_beg = 0;
+	int current_arg = 0;
+	char c;
+	
+	if(argc == 1){//command to execute is echo
+		final_arr[0] = "echo";
+		argc_new = 2;
 	}
 	else{//command to execute is argv[1]
-		final_arr[0]=argv[1];
+		final_arr[0] = argv[1];
 	}
-  for(int j=1;j<argc-1;++j){
-  	final_arr[j]=argv[j+1]; 
-  } 
 	
-		int word_beg=0;
-		char c;
-		int i=0;
-		while ( read(0,&c,1)){//while reading from stdin
-			if(c==' '  || c=='\n' || c=='\t'){//first space after end of word
-				if( word_beg==1 ){
-						word_beg=0;
-						stdin[i]=buff.copy_arr(&buff);
-						i++;
-				}
-	
-			}
-			
-			else{ //reading chars from word
-					word_beg=1;
-					buff.append(&buff,c);
-			}
-			if( i==3 ){//3 arguments colleceted, executing command
-				int pid;
-				pid=fork();
-				if( pid==0 ){
-					for(int j=0;j<=i;++j){
-						final_arr[j+argc_new-1]=stdin[j];	
-					}
-					execvp(final_arr[0],final_arr);
-				}
-				int status;
-				wait(&status);
-				i=0;
-				for(int k=0;k<i;++k){
-					free(stdin[k]);
-				}
+  	for(int j = 1 ; j < argc - 1; ++j)
+  		final_arr[j] = argv[j + 1]; 
 
+	
+	while ( read(0, &c, 1) ){//while reading from stdin
+		if(c == ' '  || c == '\n' || c == '\t'){//first space after end of word
+			if( word_beg == 1 ){
+				word_beg = 0;
+				stdin[current_arg] = buff.copy_arr(&buff);
+				++current_arg;
 			}
 		}
 		
-		if(i!=0){//executing command once more if less than 3 arguments left
+		else{ //reading chars from word
+			word_beg = 1;
+			buff.append(&buff, c);
+		}
 			
-				int pid;
-				pid=fork();
-				stdin[i]=(char*)NULL;
-				if( pid==0 ){
-					for(int j=0;j<=i;++j){
-						final_arr[j+argc_new-1]=stdin[j];	
-					}
-					execvp(final_arr[0],final_arr);
-				}
-				int status;
-				wait(&status);
-				i=0;
-				for(int k=0;k<i;++k){
-						free(stdin[k]);
-				}
-		}	
+		if( current_arg == 3 )//3 arguments colleceted, executing command
+			exec_command(final_arr, stdin, &current_arg, argc_new);
+	}	
 	
+	
+	if( current_arg != 0 ){ //executing command once more if less than 3 arguments left
+		stdin[current_arg] = (char*) NULL;
+		exec_command(final_arr, stdin, &current_arg, argc_new);
+	}
 }
-
-
